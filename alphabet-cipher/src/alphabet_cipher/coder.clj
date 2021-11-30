f7cbddd9203eadcd19f65328583841fb9e11b2ee
hjjang1025 <hyojin.jang@greenlabs.co.kr>
2021-12-01 01:45:40 +0900

    week4/day1

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,14 @@
 (ns alphabet-cipher.coder)
 
 (defn encode [keyword message]
-  "encodeme")
+  (let [len (max (count keyword) (count message))
+        keyword-idxs (take len (cycle (map #(- (int %) 97)  keyword)))
+        message-idxs (take len (cycle (map #(- (int %) 97)  message)))
+        cipher-idxs (map + keyword-idxs message-idxs)
+        encode-alphabet (fn [idx] (nth (cycle "abcdefghijklmnopqrstuvwxyz") idx))]
+    (->> (map encode-alphabet cipher-idxs)
+         (apply str))))
+
 
 (defn decode [keyword message]
   "decodeme")
