d84492a63b8cda3856abd6fad63a9f8de2bf0e58
hjjang1025 <hyojin.jang@greenlabs.co.kr>
2021-12-03 00:50:49 +0900

    week4/day3

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,29 +1,35 @@
 (ns alphabet-cipher.coder)
 
+(defn get-idxs [len text]
+  (take len (cycle (map #(- (int %) 97)  text))))
+
+(defn idx->alphabet [idx]
+  (nth (cycle "abcdefghijklmnopqrstuvwxyz") idx))
+
 (defn encode [keyword message]
   (let [len (max (count keyword) (count message))
-        keyword-idxs (take len (cycle (map #(- (int %) 97)  keyword)))
-        message-idxs (take len (cycle (map #(- (int %) 97)  message)))
-        encoded-idxs (map + keyword-idxs message-idxs)
-        idx->alphabet (fn [idx] (nth (cycle "abcdefghijklmnopqrstuvwxyz") idx))]
+        keyword-idxs (get-idxs len keyword)
+        message-idxs (get-idxs len message)
+        encoded-idxs (map + keyword-idxs message-idxs)]
     (->> (map idx->alphabet encoded-idxs)
          (apply str))))
 
-
 (defn decode [keyword cipher]
   (let [len (max (count keyword) (count cipher))
-        keyword-idxs (take len (cycle (map #(- (int %) 97)  keyword)))
-        cipher-idxs (take len (cycle (map #(- (int %) 97)  cipher)))
-        decoded-idxs (map (partial + 26) (map - cipher-idxs keyword-idxs))
-        idx->alphabet (fn [idx] (nth (cycle "abcdefghijklmnopqrstuvwxyz") idx))]
+        keyword-idxs (get-idxs len keyword)
+        cipher-idxs (get-idxs len cipher)
+        decoded-idxs (map (partial + 26) (map - cipher-idxs keyword-idxs))]
     (->> (map idx->alphabet decoded-idxs)
          (apply str))))
 
 
 (defn get-cycle [coll]
   (let [len (count coll)]
-    (->> (keep-indexed (fn [idx _] (when (= coll
-                                            (apply str (take len (cycle (subs coll 0 (inc idx)))))) (subs coll 0 (inc idx)))) coll)
+    (->> (keep-indexed (fn [idx _]
+                         (when (= coll
+                                  (->> (cycle (subs coll 0 (inc idx)))
+                                       (take len)
+                                       (apply str))) (subs coll 0 (inc idx)))) coll)
          (first))))
 
 
--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,14 @@
 (ns wonderland-number.finder)
 
+(defn wonderland-number? [x]
+  (letfn [(hasAllTheSameDigits? [n1 n2]
+           (let [s1 (set (str n1))
+                 s2 (set (str n2))]
+             (= s1 s2)))]
+    (let [multiplied-list (map (partial * x) [2 3 4 5 6])]
+      (= 5 (count (filter #(hasAllTheSameDigits? x %) multiplied-list))))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (let [number-range (range 100000 166666)]
+    (->> (filter wonderland-number? number-range)
+         (first))))
