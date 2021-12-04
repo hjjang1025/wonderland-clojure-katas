88a688ec597dd9edc7a91d744c2180035be11359
hjjang1025 <hyojin.jang@greenlabs.co.kr>
2021-12-05 02:36:21 +0900

    week4/day5 테스트 통과

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -3,4 +3,21 @@
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
 (defn river-crossing-plan []
-  start-pos)
+  [[[:fox :goose :corn :you] [:boat] []]
+   [[:fox :corn] [:boat :you :goose] []]
+   [[:fox :corn] [:boat] [:you :goose]]
+   [[:fox :corn] [:boat :you] [:goose]]
+   [[:fox :corn :you] [:boat] [:goose]]
+   [[:fox] [:boat :you :corn] [:goose]]
+   [[:fox] [:boat] [:you :goose :corn]]
+   [[:fox] [:boat :you :goose] [:corn]]
+   [[:fox :goose :you] [:boat] [:corn]]
+   [[:goose] [:boat :you :fox] [:corn]]
+   [[:goose] [:boat] [:corn :you :fox]]
+   [[:goose] [:boat :you] [:fox :corn]]
+   [[:goose :you] [:boat] [:fox :corn]]
+   [[] [:boat :goose :you] [:fox :corn]]
+   [[] [:boat] [:fox :goose :corn :you]]])
+
+
+
